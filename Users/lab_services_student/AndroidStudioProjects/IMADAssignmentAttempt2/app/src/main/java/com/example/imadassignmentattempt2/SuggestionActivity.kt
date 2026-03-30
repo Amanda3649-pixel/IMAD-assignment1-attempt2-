e13ccd3d7d1a210674c22294c88588d21fe9a733
Amanda3649-pixel <st10485255@myemeris.edu.za>
2026-03-30 22:39:06 +0200

    third commit (added a button to get suggestion list)

--- Users/lab_services_student/AndroidStudioProjects/IMADAssignmentAttempt2/app/src/main/java/com/example/imadassignmentattempt2/SuggestionActivity.kt
+++ Users/lab_services_student/AndroidStudioProjects/IMADAssignmentAttempt2/app/src/main/java/com/example/imadassignmentattempt2/SuggestionActivity.kt
@@ -1,6 +1,9 @@
 package com.example.imadassignmentattempt2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,26 @@ class SuggestionActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val name = intent.getStringExtra("name")
+        val time = intent.getStringExtra("time")
+        var tvSuggestionDisplay = findViewById<TextView>(R.id.tvSuggestionDisplay)
+        val btnBefore = findViewById<Button>(R.id.btnNext)
+        val btnLeave = findViewById<Button>(R.id.btnLeave)
+        val btnGetSuggestion = findViewById<Button>(R.id.btnGetSuggestion)
+
+
+        btnBefore.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+        }
+        btnLeave.setOnClickListener {
+            finish()
+        }
+        btnGetSuggestion.setOnClickListener {
+            
+
+
+        }
+
+
     }
 }
