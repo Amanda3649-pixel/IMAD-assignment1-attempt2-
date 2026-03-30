4764729c2171227e7216672174ec608f79b44c6b
Amanda3649-pixel <st10485255@myemeris.edu.za>
2026-03-30 22:25:04 +0200

    second commit (code for buttons added)

--- Users/lab_services_student/AndroidStudioProjects/IMADAssignmentAttempt2/app/src/main/java/com/example/imadassignmentattempt2/MainActivity.kt
+++ Users/lab_services_student/AndroidStudioProjects/IMADAssignmentAttempt2/app/src/main/java/com/example/imadassignmentattempt2/MainActivity.kt
@@ -1,6 +1,9 @@
  package com.example.imadassignmentattempt2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var edtEnterName = findViewById<EditText>(R.id.edtEnterName)
+        var edtTimeOfDay = findViewById<EditText>(R.id.edtTimeOfDay)
+        val btnNext = findViewById<Button>(R.id.btnNext)
+        val btnLeave = findViewById<Button>(R.id.btnLeave)
+
+        btnNext.setOnClickListener {
+            val name = edtEnterName.text.toString()
+            val time = edtTimeOfDay.text.toString()
+            val intent = Intent(this, SuggestionActivity::class.java)
+            intent.putExtra("name", name)
+            intent.putExtra("time", time)
+            startActivity(intent)
+        }
+        //https://github.com/liehanels/MultiScreenApp/blob/master/app/src/main/java/com/example/multiscreenapp/MainActivity.kt
+        //reference for this code
+
+        btnLeave.setOnClickListener {
+            finish()
+        }
+        //https://github.com/liehanels/MultiScreenApp/blob/master/app/src/main/java/com/example/multiscreenapp/MainActivity.kt
+        //reference for this code
+
+
+
+
+
     }
 }
